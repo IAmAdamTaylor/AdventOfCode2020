a39ed18374c67d70ef5cbc50a3e24d778c37cf1d
Adam Taylor <AdamTaylor@core-marketing.co.uk>
2020-12-01 21:35:56 +0000

    Solve Day 1 Part 2

--- solutions/day1-part2.js
+++ solutions/day1-part2.js
@@ -1,11 +1,11 @@
 /**
  * Advent of Code 2020
- * Day 1 - Part 1
+ * Day 1 - Part 2
  *
  * From the given list of numbers, find the
- * 2 that sum to 2020 and return their product.
+ * 3 that sum to 2020 and return their product.
  * 
- * @see https://github.com/IAmAdamTaylor/AdventOfCode2020/wiki/Day-1---Part-1
+ * @see https://github.com/IAmAdamTaylor/AdventOfCode2020/wiki/Day-1---Part-2
  */
 
 // set the number we want to find
@@ -19,27 +19,34 @@ const INPUT = readLines('../inputs/day1.txt', function(data) {
 		.sort((a, b) => a - b); // sort them into numerical order
 });
 
-// get start and end indexes
+// get start, 2nd item and end indexes
 let i = 0;
-let j = INPUT.length - 1;
-let sum = INPUT[i] + INPUT[j]; // set intial loop var
+let j = 1;
+let k = INPUT.length - 1;
+let sum = INPUT[i] + INPUT[j] + INPUT[k]; // set intial loop var
 
 // loop until sum equals the TARGET
 while ( sum !== TARGET ) {
-	// if less than TARGET, advance the lower index by 1
+	// if less than TARGET, advance one of the lower indexes by 1
 	if ( sum < TARGET ) {
-		i++;
+		if ( j - i === 1 ) {
+			// advance j when it is adjacent to i
+			j++;
+		} else {
+			// otherwise advance i (move closer to j)
+			i++;
+		}
 	}
 
 	// if greater than TARGET, retreat the higher index by 1
 	if ( sum > TARGET ) {
-		j--;
+		k--;
 	}
 
-	sum = INPUT[i] + INPUT[j];
+	sum = INPUT[i] + INPUT[j] + INPUT[k];
 }
 
-// log both found numbers
-console.log( INPUT[i], INPUT[j] );
+// log all found numbers
+console.log( INPUT[i], INPUT[j], INPUT[k] );
 // Answer is their product (multiply numbers together)
-console.log( INPUT[i] * INPUT[j] );
+console.log( INPUT[i] * INPUT[j] * INPUT[k] );
