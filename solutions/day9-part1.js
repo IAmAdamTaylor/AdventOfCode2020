b76941218505c08c422c69657a80b7c2f332fbdd
Adam Taylor <AdamTaylor@core-marketing.co.uk>
2020-12-09 22:13:28 +0000

    Solve Day 9 Part 1

---
+++ solutions/day9-part1.js
@@ -0,0 +1,51 @@
+/**
+ * Advent of Code 2020
+ * Day 9 - Part 1
+ *
+ * Find the first line with a number that is not a sum of
+ * 2 distinct numbers from the 25 previous lines.
+ * 
+ * @see https://github.com/IAmAdamTaylor/AdventOfCode2020/wiki/9.1:-Day-9-Part-1
+ */
+
+const {readLines} = require('adventOfCode');
+const INPUT = readLines('inputs/day9.txt', (data) => {
+	return data.filter((x) => x); // remove blank lines
+});
+
+/**
+ * Calculate every possible sum of numbers from arr.
+ * @param  {Array} arr An array of numbers.
+ * @return {Array}     
+ */
+function enumSums( arr ) {
+	let sums = [];
+
+	arr.forEach(a => {
+		arr.forEach(b => {
+			if ( a != b ) {
+				sums.push(parseInt(a) + parseInt(b));
+			}
+		});
+	});
+
+	return sums;
+}
+
+let answer = (() => {
+	for (let i = 25, len = INPUT.length; i < len; i++) {
+		let num = parseInt(INPUT[i]);
+		let preamble = INPUT.slice(i - 25, i);
+
+		console.log( enumSums(preamble).length );
+
+		if ( !enumSums(preamble).includes(num) ) {
+			return num;
+		}
+	}
+})();
+
+// export part 1 for use in part 2
+module.exports.ANSWER = answer;
+
+console.log( answer );
